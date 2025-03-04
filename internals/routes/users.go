415d308e58b2deaaa11dbf060c4ab75345e04c7b
Adejare <adejare.arasheed@gmail.com>
2025-03-04 12:56:50 +0100

    user's routes

---
+++ internals/routes/users.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/Adejare77/blogPlatform/internals/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+var UserRoutes = func(r *gin.RouterGroup) {
+	r.POST("/register", controllers.Register)
+	r.POST("/login", controllers.Login)
+	r.GET("/index", controllers.AllPosts)
+}
