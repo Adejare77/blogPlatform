0fdde75ef4ba802fef3ec81c56a3536081d5431d
Adejare <adejare.arasheed@gmail.com>
2025-03-04 12:57:01 +0100

    Posts routes

---
+++ internals/routes/posts.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/Adejare77/blogPlatform/internals/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+var PostRoutes = func(r *gin.RouterGroup) {
+	r.POST("/posts", controllers.CreatePost)
+	r.GET("/posts", controllers.MyPosts)
+	r.GET("/posts/:id", controllers.GetPost)
+	r.PUT("/posts/:id", controllers.UpdatePost)
+	r.DELETE("/posts/:id", controllers.DeletePost)
+	r.GET("/logout", controllers.Logout)
+}
