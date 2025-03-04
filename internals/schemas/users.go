09d0c7c909078b9756f3b2035ba3215270406c5a
Adejare <adejare.arasheed@gmail.com>
2025-03-04 12:58:04 +0100

    User's Database Schema

---
+++ internals/schemas/users.go
@@ -0,0 +1,21 @@
+package schemas
+
+import (
+	"github.com/Adejare77/blogPlatform/internals/utilities"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID       uint      `gorm:"primaryKey"`
+	Name     string    `gorm:"not null" binding:"required"`
+	Email    string    `gorm:"unique;not null" binding:"required,email"`
+	Password string    `gorm:"not null" binding:"required"`
+	Posts    []Post    `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+	Likes    []Like    `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+	Comments []Comment `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+}
+
+func (newUser *User) BeforeCreate(tx *gorm.DB) (err error) {
+	newUser.Password, err = utilities.HashPassword(newUser.Password)
+	return
+}
