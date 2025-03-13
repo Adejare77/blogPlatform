b6657e43d32afb643158c33414373b9c6a248eea
Rashisky <rashisky007@gmail.com>
2025-03-13 19:57:29 +0100

    Update like schema; use Author ID for post and comment, remove User field

--- internals/schemas/likes.go
+++ internals/schemas/likes.go
@@ -3,10 +3,9 @@ package schemas
 import "time"
 
 type Like struct {
-	ID           uint   `gorm:"primaryKey"`
-	UserID       uint   `gorm:"not null"`
-	LikeableID   string `gorm:"type:text;not null;index:idx_likeable"`
+	AuthorID     uint   `gorm:"not null"`
+	UserID       uint   `gorm:"primaryKey;index;uniqueIndex:user_like"`
+	LikeableID   string `gorm:"type:text;not null;index:idx_likeable;uniqueIndex:user_like"`
 	LikeableType string `gorm:"not null;index:idx_likeable"`
 	CreatedAt    time.Time
-	User         User `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"` // Belongs to User
 }
