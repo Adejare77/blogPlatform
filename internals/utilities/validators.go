e30f5a199df4f71eea7a13b10e4d1cab51dcd0ba
Rashisky <rashisky007@gmail.com>
2025-03-17 09:04:30 +0100

    Update fieldError to be 'oneof'

--- internals/utilities/validators.go
+++ internals/utilities/validators.go
@@ -12,12 +12,14 @@ func Validator(ctx *gin.Context, err error) {
 	if validationError, ok := err.(validator.ValidationErrors); ok {
 		var errorDetails []string
 		for _, fieldError := range validationError {
+			fmt.Println(fieldError.Tag())
 			if fieldError.Tag() == "required" {
 				errorDetails = append(errorDetails, fmt.Sprintf("The `%s` field is required", fieldError.Field()))
-			} else if fieldError.Tag() == "status" {
-				errorDetails = append(errorDetails, fmt.Sprintf("The `%s` field can only be `draft` or `published`", fieldError.Field()))
+			} else if fieldError.Tag() == "oneof" {
+				errorDetails = append(errorDetails, "The `status` field can only be `unpublished` or `published`")
 			}
 		}
+
 		handlers.BadRequest(ctx, errorDetails, errorDetails)
 	} else {
 		handlers.BadRequest(ctx,
