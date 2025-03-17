578cf69ff27972dc06214c1504286394a06b978c
Rashisky <rashisky007@gmail.com>
2025-03-17 12:18:03 +0100

    PostID uses uuid type

--- internals/schemas/comments.go
+++ internals/schemas/comments.go
@@ -2,24 +2,16 @@ package schemas
 
 import (
 	"time"
-
-	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type Comment struct {
 	ID        string `gorm:"primaryKey"`
-	PostID    string `gorm:"not null"`
+	PostID    string `gorm:"type:uuid;not null"`
 	UserID    uint   `gorm:"not null"`
 	Content   string `gorm:"type:text" binding:"required"`
 	CreatedAt time.Time
-	ParentID  *string   `gorm:"index;default:null"`
-	Likes     []Like    `gorm:"polymorphic:Likeable;polymorphicValue:Comment;constraint:OnDelete:CASCADE"`
+	ParentID  *string   `gorm:"type:uuid;index;default:null"`
+	Likes     []Like    `gorm:"type:uuid;polymorphic:Likeable;polymorphicValue:Comment;constraint:OnDelete:CASCADE"`
 	User      User      `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 	Replies   []Comment `gorm:"foreignKey:ParentID"`
-}
-
-func (comment *Comment) BeforeCreate(tx *gorm.DB) (err error) {
-	comment.ID = uuid.New().String()
-	return
 }
