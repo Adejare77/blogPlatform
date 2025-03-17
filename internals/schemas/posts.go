8163f8bd195bd8b0e1d4896785b4fbeee16594db
Rashisky <rashisky007@gmail.com>
2025-03-17 12:18:42 +0100

    ID uses in-built uuid type

--- internals/schemas/posts.go
+++ internals/schemas/posts.go
@@ -2,13 +2,10 @@ package schemas
 
 import (
 	"time"
-
-	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type Post struct {
-	ID        string `gorm:"primaryKey"`
+	ID        string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	AuthorID  uint   `gorm:"not null;index;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Title     string `binding:"required"`
 	Content   string `gorm:"type:text" binding:"required"`
@@ -18,9 +15,4 @@ type Post struct {
 	Likes     []Like    `gorm:"polymorphic:Likeable;polymorphicValue:Post;constraint:OnDelete:CASCADE"`
 	Comments  []Comment `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE"`
 	User      User      `gorm:"foreignKey:AuthorID;references:ID;constraint:OnDelete:CASCADE" binding:"-"`
-}
-
-func (post *Post) BeforeCreate(tx *gorm.DB) (err error) {
-	post.ID = uuid.New().String()
-	return
 }
