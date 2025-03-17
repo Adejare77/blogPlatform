ca2776d636d5cc671868f8344b9a44603b760c4f
Rashisky <rashisky007@gmail.com>
2025-03-17 12:19:18 +0100

    Likeable ID type changed to uuid

--- internals/schemas/likes.go
+++ internals/schemas/likes.go
@@ -5,7 +5,7 @@ import "time"
 type Like struct {
 	AuthorID     uint   `gorm:"not null"`
 	UserID       uint   `gorm:"primaryKey;index;uniqueIndex:user_like"`
-	LikeableID   string `gorm:"type:text;not null;index:idx_likeable;uniqueIndex:user_like"`
+	LikeableID   string `gorm:"type:uuid;not null;index:idx_likeable;uniqueIndex:user_like"`
 	LikeableType string `gorm:"not null;index:idx_likeable"`
 	CreatedAt    time.Time
 }
