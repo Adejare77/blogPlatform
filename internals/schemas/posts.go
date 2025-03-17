a61ba790b7d14154a9b7811a7d269f7906e72c53
Rashisky <rashisky007@gmail.com>
2025-03-17 09:02:12 +0100

    Status field to accept only 'unpublished' or 'published' blog

--- internals/schemas/posts.go
+++ internals/schemas/posts.go
@@ -12,7 +12,7 @@ type Post struct {
 	AuthorID  uint   `gorm:"not null;index;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Title     string `binding:"required"`
 	Content   string `gorm:"type:text" binding:"required"`
-	Status    string `gorm:"default:'draft'" binding:"omitempty,oneof=draft published"`
+	Status    string `gorm:"default:'draft'" binding:"required,oneof=unpublished published"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	Likes     []Like    `gorm:"polymorphic:Likeable;polymorphicValue:Post;constraint:OnDelete:CASCADE"`
