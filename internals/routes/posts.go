068fb67278cc69bc2f414573a2ad7e28e9ba828a
Rashisky <rashisky007@gmail.com>
2025-03-18 22:10:54 +0100

    Include drafts routes

--- internals/routes/posts.go
+++ internals/routes/posts.go
@@ -8,8 +8,10 @@ import (
 var PostRoutes = func(r *gin.RouterGroup) {
 	r.POST("/posts", controllers.CreatePost)
 	r.GET("/posts", controllers.MyPosts)
-	r.GET("/posts/:id", controllers.GetPost)
+	r.GET("/posts/drafts", controllers.MyDrafts)
+	r.GET("/posts/drafts/:id", controllers.GetDraft)
 	r.PUT("/posts/:id", controllers.UpdatePost)
+	r.PUT("/posts/drafts/:id", controllers.UpdatePost)
 	r.DELETE("/posts/:id", controllers.DeletePost)
 	r.GET("/logout", controllers.Logout)
 }
