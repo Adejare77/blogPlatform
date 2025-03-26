132b15d6ce37017f32ec8a498eeb2eed841618a9
Rashisky <rashisky007@gmail.com>
2025-03-26 21:47:48 +0100

    add structs: PostURIParams and StatusQueryParams

--- internals/schemas/posts.go
+++ internals/schemas/posts.go
@@ -5,14 +5,21 @@ import (
 )
 
 type Post struct {
-	ID        string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	AuthorID  uint   `gorm:"not null;index;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Title     string `binding:"required"`
-	Content   string `gorm:"type:text" binding:"required"`
-	Status    string `gorm:"default:'draft'" binding:"required,oneof=unpublished published"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	AuthorID  uint      `gorm:"not null;index;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Title     string    `binding:"required"`
+	Content   string    `gorm:"type:text" binding:"required"`
+	Status    string    `gorm:"default:'draft'" binding:"omitempty,oneof=draft published"`
 	Likes     []Like    `gorm:"polymorphic:Likeable;polymorphicValue:Post;constraint:OnDelete:CASCADE"`
 	Comments  []Comment `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE"`
-	User      User      `gorm:"foreignKey:AuthorID;references:ID;constraint:OnDelete:CASCADE" binding:"-"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type PostURIParams struct {
+	PostID string `uri:"post_id" binding:"required,uuid"`
+}
+
+type StatusQueryParams struct {
+	Status string `form:"status" binding:"required,oneof=draft published"`
 }
