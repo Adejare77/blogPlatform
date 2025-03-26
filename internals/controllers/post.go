5d1b63549f949e638d9fd8b4fb8424b21f486415
Rashisky <rashisky007@gmail.com>
2025-03-26 21:35:23 +0100

    Improved response structure; updated business logic

--- internals/controllers/post.go
+++ internals/controllers/post.go
@@ -1,13 +1,17 @@
 package controllers
 
 import (
+	"errors"
 	"net/http"
+	"strings"
 
+	"github.com/Adejare77/blogPlatform/config"
 	"github.com/Adejare77/blogPlatform/internals/handlers"
 	"github.com/Adejare77/blogPlatform/internals/models"
 	"github.com/Adejare77/blogPlatform/internals/schemas"
-	"github.com/Adejare77/blogPlatform/internals/utilities"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 func CreatePost(ctx *gin.Context) {
@@ -16,7 +20,7 @@ func CreatePost(ctx *gin.Context) {
 	var post schemas.Post
 
 	if err := ctx.ShouldBind(&post); err != nil {
-		utilities.Validator(ctx, err)
+		handlers.Validator(ctx, err)
 		return
 	}
 
@@ -27,90 +31,197 @@ func CreatePost(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusAccepted, gin.H{
-		"title":   post.Title,
-		"content": post.Content[:150] + "...",
-		"status":  post.Status,
+	if err := config.IncrementTotalPosts(); err != nil {
+		logrus.WithFields(logrus.Fields{
+			"Warning": "Failed to Increase Total blog Posts",
+		})
+	}
+	ctx.JSON(http.StatusCreated, gin.H{
+		"status":  http.StatusCreated,
+		"message": "post created successfully",
 	})
 }
 
-func AllPosts(ctx *gin.Context) {
-	posts, err := models.GetAllPosts()
+func GetAllPosts(ctx *gin.Context) {
+	posts, err := models.FindAllPosts()
 	if err != nil {
 		handlers.InternalServerError(ctx, err)
 		return
 	} else if len(posts) == 0 {
-		ctx.JSON(http.StatusNotFound, "No Record Found")
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"status":  http.StatusNotFound,
+			"message": "no record found",
+		})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, posts)
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": posts,
+		"meta": gin.H{
+			"total_posts": config.TotalPosts,
+		},
+	})
 }
 
-func MyPosts(ctx *gin.Context) {
-	userID := ctx.MustGet("currentUser").(uint)
+func GetUserPosts(ctx *gin.Context) {
+	currentUser := ctx.MustGet("currentUser").(uint)
+	var status schemas.StatusQueryParams
 
-	posts, err := models.GetPosts(userID)
+	status.Status = "published" // Default if not given
+	if err := ctx.ShouldBindQuery(&status); err != nil {
+		handlers.Validator(ctx, err)
+		return
+	}
+
+	posts, err := models.FindUserPosts(currentUser, status.Status)
 	if err != nil {
 		handlers.InternalServerError(ctx, err)
 		return
 	}
 
 	if len(posts) == 0 {
-		ctx.JSON(http.StatusNotFound, "No Record Found")
+		ctx.JSON(http.StatusOK, []map[string]any{})
 		return
 	}
 
 	ctx.JSON(http.StatusOK, posts)
 }
 
-func GetPost(ctx *gin.Context) {
-	ctx.MustGet("currentUser")
-	postID := ctx.Param("id")
+func GetPostByID(ctx *gin.Context) {
+	var post schemas.PostURIParams
+	var status schemas.StatusQueryParams
+	var currentUser uint // Defaults to 0 (No user)
 
-	post, err := models.GetPostByID(postID)
+	if err := ctx.ShouldBindUri(&post); err != nil {
+		handlers.Validator(ctx, err)
+		return
+	}
 
+	status.Status = "published" // Defaults to published
+
+	if err := ctx.ShouldBindQuery(&status); err != nil {
+		handlers.Validator(ctx, err)
+		return
+	}
+
+	if status.Status == "draft" {
+		userID, err := getCurrentUser(ctx)
+		if err != nil {
+			handlers.Unauthorized(ctx, "login required", "Access to Unauthorized files")
+			return
+		}
+		currentUser = userID
+	}
+
+	result, err := models.FindByPostID(currentUser, post.PostID, status.Status)
 	if err != nil {
-		utilities.Validator(ctx, err)
-		return
-	}
-
-	if len(post) == 0 {
-		ctx.JSON(http.StatusOK, "Record Not Found")
-		return
-	}
-
-	ctx.JSON(http.StatusOK, post)
-}
-
-func UpdatePost(ctx *gin.Context) {
-	userID := ctx.MustGet("currentUser").(uint)
-	postID := ctx.Param("id")
-
-	var data map[string]any
-	if err := ctx.ShouldBind(&data); err != nil {
-		utilities.Validator(ctx, err)
-		return
-	}
-
-	data["userID"] = userID
-	data["postID"] = postID
-	if err := models.UpdatePost(data); err != nil {
 		handlers.InternalServerError(ctx, err)
 		return
 	}
+	if len(result) == 0 {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "no record found",
+		})
+		return
+	}
 
-	ctx.Redirect(http.StatusSeeOther, postID)
+	ctx.JSON(http.StatusOK, result)
+}
+
+func UpdatePost(ctx *gin.Context) {
+	AuthorID := ctx.MustGet("currentUser").(uint)
+	var post schemas.PostURIParams
+
+	if err := ctx.ShouldBindUri(&post); err != nil {
+		handlers.Validator(ctx, err)
+		return
+	}
+
+	type PostUpdateDTO struct {
+		Title   *string `json:"title" binding:"omitempty"`
+		Content *string `json:"content" binding:"omitempty"`
+		Status  *string `json:"status" binding:"omitempty,oneof=draft published"`
+	}
+
+	var dto PostUpdateDTO
+	if err := ctx.ShouldBindJSON(&dto); err != nil {
+		handlers.Validator(ctx, err)
+		return
+	}
+
+	updateData := make(map[string]any)
+	if dto.Content != nil {
+		updateData["content"] = dto.Content
+	}
+
+	if dto.Title != nil {
+		updateData["title"] = dto.Title
+	}
+
+	if dto.Status != nil {
+		updateData["status"] = dto.Status
+	}
+
+	if err := models.UpdateUserPost(AuthorID, post.PostID, updateData); err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			handlers.BadRequest(ctx, "Record not Found", err.Error())
+			return
+		}
+		if strings.Contains(err.Error(), "forbidden") {
+			handlers.Forbidden(ctx, "Forbidden", err.Error())
+			return
+		}
+
+		handlers.InternalServerError(ctx, err.Error())
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "updated successfully",
+	})
 }
 
 func DeletePost(ctx *gin.Context) {
 	userID := ctx.MustGet("currentUser").(uint)
-	postID := ctx.Param("id")
+	var post schemas.PostURIParams
 
-	if err := models.DeletePost(userID, postID); err != nil {
-		handlers.InternalServerError(ctx, err)
+	if err := ctx.ShouldBindUri(&post); err != nil {
+		handlers.Validator(ctx, err)
 		return
 	}
 
-	ctx.JSON(http.StatusOK, "Successfully Deleted")
+	if err := models.DeleteUserPost(userID, post.PostID); err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			handlers.BadRequest(ctx, "Record not Found", err.Error())
+		} else if strings.Contains(err.Error(), "forbidden") {
+			handlers.Forbidden(ctx, "Forbidden", err.Error())
+		} else {
+			handlers.InternalServerError(ctx, err.Error())
+		}
+		return
+	}
+
+	if err := config.DecrementTotalPosts(); err != nil {
+		logrus.WithField("Warning", "Failed to Decrease Total blog Posts")
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "successfully deleted",
+	})
+
+}
+
+func getCurrentUser(ctx *gin.Context) (uint, error) {
+	session := sessions.Default(ctx)
+	currentUser := session.Get("currentUser")
+	if currentUser == nil {
+		return 0, errors.New("unauthenticated")
+	}
+
+	session.Set("currentUser", currentUser)
+
+	return currentUser.(uint), session.Save()
 }
