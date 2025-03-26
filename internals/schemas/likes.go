b82852de6169e530c7063315bbccd4af2fd137a1
Rashisky <rashisky007@gmail.com>
2025-03-26 21:48:51 +0100

    remove primaryKey tag from UserID. Added struct LikedQueryParams

--- internals/schemas/likes.go
+++ internals/schemas/likes.go
@@ -3,9 +3,12 @@ package schemas
 import "time"
 
 type Like struct {
-	AuthorID     uint   `gorm:"not null"`
-	UserID       uint   `gorm:"primaryKey;index;uniqueIndex:user_like"`
+	UserID       uint   `gorm:"index;uniqueIndex:user_like"`
 	LikeableID   string `gorm:"type:uuid;not null;index:idx_likeable;uniqueIndex:user_like"`
 	LikeableType string `gorm:"not null;index:idx_likeable"`
 	CreatedAt    time.Time
+}
+
+type LikedQueryParams struct {
+	Type string `form:"type" binding:"required,oneof=post comment"`
 }
