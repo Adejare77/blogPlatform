e980763e94be6925def7114cfdb5a1df9d50a1a5
Rashisky <rashisky007@gmail.com>
2025-03-26 21:41:10 +0100

    Improved response columns. query using status value

--- internals/models/likes.go
+++ internals/models/likes.go
@@ -1,40 +1,62 @@
 package models
 
 import (
-	"errors"
-
 	"github.com/Adejare77/blogPlatform/config"
 	"github.com/Adejare77/blogPlatform/internals/schemas"
 )
 
-func GetLikedPosts(userID uint) ([]map[string]string, error) {
+func FindLikesByUser(userID uint, targetType string) ([]map[string]string, error) {
 	var result []map[string]string
 
-	if err := config.DB.Model(&schemas.Like{}).
-		Select("users.name, posts.title, LEFT(posts.content, 200) AS content").
-		Joins("LEFT JOIN posts ON posts.id = likes.likeable_id").
-		Joins("LEFT JOIN users ON likes.author_id = users.id").
-		Find(&result).Error; err != nil {
-		return nil, err
+	query := config.DB.Model(&schemas.Like{}).
+		Where("user_id = ? AND likeable_type = ?", userID, targetType)
+
+	if targetType == "post" {
+		err := query.Select(`
+		posts.id AS post_id,
+		posts.title AS post_title,
+		CONCAT(LEFT(posts.content, 200), '...') AS post_excerpt,
+		posts.created_at AS post_created_at,
+		users.name AS post_author,
+		`).
+			Joins(`INNER JOIN posts ON posts.id = likes.likeable_id`).
+			Joins(`INNER JOIN users ON likes.author_id = users.id`).
+			Find(&result).Error
+
+		if err != nil {
+			return nil, err
+		}
+
+	} else {
+		err := query.Select(`
+		comments.id AS comment_id,
+		CONCAT(LEFT(comments.content, 100), '...') AS comment_excerpt,
+		comment_user.name AS comment_author,
+		comments.created_at AS comment_created_at
+		posts.title AS parent_post_title,
+		posts.id AS parent_post_id,
+		post_user.name AS post_author,
+		`).
+			Joins(`INNER JOIN comments ON comments_id = likes.likeable_ids`).
+			Joins(`INNER JOIN posts ON posts.id = comments.post_id`).
+			Joins(`INNER JOIN users AS comment_user ON comments.author_id = users.id`).
+			Joins(`INNER JOIN users AS post_user ON posts.author_id = users.id`).
+			Find(&result).Error
+
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return result, nil
 }
 
-func LikePostOrComment(data schemas.Like) error {
-	return config.DB.FirstOrCreate(&data).Error
+func CreateLike(data schemas.Like) error {
+	return config.DB.Create(&data).Error
 }
 
-func UnlikePostOrComment(like schemas.Like) error {
-	cursor := config.DB.Delete(&like)
-
-	if cursor.Error != nil {
-		return cursor.Error
-	}
-
-	if cursor.RowsAffected == 0 {
-		return errors.New("like not found")
-	}
-
-	return nil
+func DeleteLike(userID uint, targetID string) error {
+	return config.DB.
+		Where("user_id = ? AND likeableID = ?", userID, targetID).
+		Delete(&schemas.Like{}).Error
 }
