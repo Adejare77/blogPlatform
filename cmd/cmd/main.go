baedc9c5483b03b1608c450e4f7761a5fd28a2b5
Rashisky <rashisky007@gmail.com>
2025-03-26 21:22:26 +0100

    Update to include TotalPosts on start-up

--- cmd/cmd/main.go
+++ cmd/cmd/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Adejare77/blogPlatform/config"
 	"github.com/Adejare77/blogPlatform/internals/middlesware"
+	"github.com/Adejare77/blogPlatform/internals/models"
 	"github.com/Adejare77/blogPlatform/internals/routes"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -16,16 +17,25 @@ func main() {
 		log.Fatalf("Failed to Initialize Server: %v", err)
 	}
 
+	totalPosts, err := models.TotalPosts()
+	if err != nil {
+		log.Fatalf("Faild to fetch Total Posts")
+	}
+
+	config.TotalPosts = totalPosts
+
 	app := gin.Default()
 	app.Use(sessions.Sessions("blogPost", config.SessionStore))
 
-	userRoutes := app.Group("/")
 	postRoutes := app.Group("/", middlesware.AuthMiddleware())
 	likeRoutes := app.Group("/", middlesware.AuthMiddleware())
+	commentRoutes := app.Group("/", middlesware.AuthMiddleware())
+	userRoutes := app.Group("/")
 
-	routes.UserRoutes(userRoutes)
-	routes.PostRoutes(postRoutes)
 	routes.LikesRoutes(likeRoutes)
+	routes.PostRoutes(postRoutes)
+	routes.UserRoutes(userRoutes)
+	routes.CommentRoutes(commentRoutes)
 
 	log.Print("Server running successfully")
 	if err := app.Run(":3000"); err != nil {
